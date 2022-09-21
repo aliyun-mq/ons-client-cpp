4358145f2f7655d673d6ce549d3043706197d286
Zhanhui Li <lizhanhui@apache.org>
2022-09-21 11:13:28 +0800

    Make script quiet

--- tools/cmake_install_grpc.sh
+++ tools/cmake_install_grpc.sh
@@ -2,8 +2,8 @@
 TOOLS_DIR=$(dirname "$0")
 WORKSPACE=$(cd -- "$TOOLS_DIR/.." && pwd)
 cd $WORKSPACE/third_party
-wget https://shutian.oss-cn-hangzhou.aliyuncs.com/cdn/grpc/grpc_v1.48.0.tar.gz
-tar -xzvf grpc_v1.48.0.tar.gz
+wget -q https://shutian.oss-cn-hangzhou.aliyuncs.com/cdn/grpc/grpc_v1.48.0.tar.gz
+tar -xzf grpc_v1.48.0.tar.gz
 mkdir grpc_build
 cd grpc_build
 cmake -DCMAKE_BUILD_TYPE=Release -DgRPC_SSL_PROVIDER=package -DgRPC_ZLIB_PROVIDER=package -DCMAKE_INSTALL_PREFIX=$HOME/grpc ../grpc_v1.48.0
